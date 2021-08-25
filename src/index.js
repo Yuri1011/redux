d5f0eda8d971b5baf4ff3f55bbc7b3ec3091ae18
yury trypolski <yury1011.89@gmail.com>
2021-08-25 07:31:33 +0300

    start create their redux

--- src/index.js
+++ src/index.js
@@ -1,36 +1,15 @@
-import "./styles.css";
-
 const counter = document.getElementById("counter");
 const addBtn = document.getElementById("add");
 const subBtn = document.getElementById("sub");
 const asyncBtn = document.getElementById("async");
 const themeBtn = document.getElementById("theme");
 
-let state = 0;
+addBtn.addEventListener("click", () => {});
 
-function render() {
-  counter.textContent = state.toString();
-}
+subBtn.addEventListener("click", () => {});
 
-addBtn.addEventListener("click", () => {
-  state++;
-  render();
-});
+asyncBtn.addEventListener("click", () => {});
 
-subBtn.addEventListener("click", () => {
-  state--;
-  render();
-});
-
-asyncBtn.addEventListener("click", () => {
-  setTimeout(() => {
-    state++;
-    render();
-  }, 2000);
-});
-
-themeBtn.addEventListener("click", () => {
-  document.body.classList.toggle("dark");
-});
-
-render();
+// themeBtn.addEventListener("click", () => {
+//   document.body.classList.toggle("dark");
+// });
